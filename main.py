5ebfe924c02cd8624c0d1b64b3b3ad01e02ca7e4
Luiz Otavio <luiz.otav.2009@gmail.com>
2025-07-07 12:59:53 -0300

    Data - 07/07/25

--- main.py
+++ main.py
@@ -17,24 +17,24 @@ except:
     st.warning("⚠️ Logo não encontrada.")
 
 st.markdown("## 📊 Painel de Captação de Participantes")
-st.markdown("Análise geral dos estudos clínicos | Atualizado em 2025-07-04")
+st.markdown("Análise geral dos estudos clínicos | Atualizado em 2025-07-07")
 st.markdown("---")
 
 dados = {
     "Estudo": [
         "BANCO DE DADOS",
         "ALOGLIPTINA + CLORIDRATO DE PIOGLITAZONA",
-        "AZILSARTANA MEDOXOMILA 3ºG",
+        "BD POMADA",
+        "BREXPIPRAZOL",
         "CLORIDRATO DE NEBIVOLOL 1ºG",
-        "CLORIDRATO DE NEBIVOLOL 2ºG",
         "CLORIDRATO DE PAZOPANIBE",
         "ROSUVASTATINA CÁLCICA + EZETIMIBA",
         "SUMATRIPTANA + NAPROXENO"
     ],
-    "Inaptos_Homens":       [0, 0, 0, 0, 0, 0, 0, 0],
-    "Inaptos_Mulheres":     [0, 0, 0, 0, 0, 1, 0, 0],
-    "Aptos_Homens":         [4, 3, 5, 0, 0, 2, 1, 1],
-    "Aptos_Mulheres":       [3, 1, 0, 6, 1, 6, 0, 1],
+    "Inaptos_Homens":       [1, 0, 1, 1, 0, 1, 0, 1],
+    "Inaptos_Mulheres":     [0, 1, 1, 0, 0, 2, 0, 0],
+    "Aptos_Homens":         [3, 0, 0, 3, 0, 3, 3, 0],
+    "Aptos_Mulheres":       [2, 3, 0, 0, 1, 3, 2, 1],
     "Desistentes_Homens":   [0, 0, 0, 0, 0, 0, 0, 0],
     "Desistentes_Mulheres": [0, 0, 0, 0, 0, 0, 0, 0]
 }
@@ -52,7 +52,7 @@ total_desistentes = df["Desistentes_Total"].sum()
 total_geral = df["Total_Geral"].sum()
 taxa_inaptos = (total_inaptos / total_geral) * 100
 
-total_agendados = 146
+total_agendados = 131
 total_faltaram = total_agendados - df["Total_Geral"].sum()
 total_compareceram = total_agendados - total_faltaram
 taxa_comparecimento = (total_compareceram / total_agendados) * 100
